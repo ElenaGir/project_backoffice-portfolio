e158c926555625649adddf43fed6de589959a2d9
ElenaGir <elena.girard@gmail.com>
2022-04-13 16:37:32 +0200

    adding a form page

--- handler_add-project.php
+++ handler_add-project.php
@@ -1,14 +1,23 @@
 <?php
-// cleaning and storing data in variables
- $project_title = strip_tags($_POST["data_title"]);
- $project_description = strip_tags($_POST["data_description"]);
-// insert data in database
-require_once("db_connection.php");
-$sql = 'INSERT INTO `table_projects` (`project_title`, `project_description`) VALUES (:project_title, :project_description);';
-$query =  $db->prepare($sql);
-$query->bindValue(':project_title', $project_title, PDO::PARAM_STR);
-$query->bindValue(':project_description', $project_description, PDO::PARAM_STR);
-$query -> execute();
-//redirection 
-echo '<div>Project added.</div>';
-echo '<div><a href="index.php"><button>Back</button></a></div>';
+    // Verifying form fields
+    if(isset($_POST['data_title']) && !empty($_POST['data_title'])
+    && isset($_POST['data_description']) && !empty($_POST['data_description'])){
+        // cleaning and storing data in variables
+        $project_title = strip_tags($_POST["data_title"]);
+        $project_description = strip_tags($_POST["data_description"]);
+        // insert data in database
+        require_once("db_connection.php");
+        $sql = 'INSERT INTO `table_projects` (`project_title`, `project_description`) VALUES (:project_title, :project_description);';
+        $query =  $db->prepare($sql);
+        $query->bindValue(':project_title', $project_title, PDO::PARAM_STR);
+        $query->bindValue(':project_description', $project_description, PDO::PARAM_STR);
+        $query -> execute();
+        //redirection 
+        echo '<div>Project added.</div>';
+        echo '<div><a href="index.php"><button>Back</button></a></div>';
+
+        //If the form fields are empty
+    } else {
+        echo "Complete all fields. ";
+        echo '<div><a href="index.php"><button>Back</button></a></div>';
+    }
