d124249a230e434e034605e38d44fd0b43a7ecbd
ElenaGir <elena.girard@gmail.com>
2022-04-13 17:02:58 +0200

    loop on the list of project

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
-<?php include 'include_header.php' ?>
+<?php
+    require_once('db_connection.php');
+    $sql = 'SELECT * FROM `table_projects`';
+    $query = $db->prepare($sql);
+    $query->execute();
+    $projects = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php include 'header.php' ?>
+
+<div>
+    <?php foreach($projects as $project){ ?>
+        <div>
+            <?= $project['project_title'] ?> 
+        </div>
+    <?php } ?>
+</div>
 
 <div>
     <a href="form_project-add.php">
         <button>Add project</button>
     </a>
 </div>
-
-<?php include 'include_footer.php' ?>
+<?php include 'footer.php' ?>
     
